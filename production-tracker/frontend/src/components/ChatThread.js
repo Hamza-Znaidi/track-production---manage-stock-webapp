250da9907656280691591543194f0f7ad15edf69
Hamza Znaidi <hhznaidii@gmail.com>
2026-03-11 10:42:16 +0100

    Update chatThread

--- production-tracker/frontend/src/components/ChatThread.js
+++ production-tracker/frontend/src/components/ChatThread.js
@@ -244,11 +244,11 @@ export default function ChatThread({ threadId, currentUserId, onClose }) {
 
         {/* Typing indicator */}
         {typingUsers.size > 0 && (
-          <div className="bg-gary-200">
+          <div className="bg-gray-200 px-3 py-1 rounded-full w-max mx-auto flex items-center gap-1">
 
-            <div className="h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:0.3s]"></div>
-            <div className="h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:0.1s]"></div>
-            <div className="h-4 w-4 bg-black rounded-full animate-bounce "></div>
+            <div className="h-1 w-1 bg-black rounded-full animate-bounce [animation-delay:0.3s]"></div>
+            <div className="h-1 w-1 bg-black rounded-full animate-bounce [animation-delay:0.1s]"></div>
+            <div className="h-1 w-1 bg-black rounded-full animate-bounce "></div>
           </div>
         )}
 
